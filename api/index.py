bad8d63c7ae47d7c79c21e34edd10fd6c0f9f618
Ashtonex <77593052+Ashtonex@users.noreply.github.com>
2026-05-14 17:30:35 +0200

    fix: add root requirements.txt and fix python module pathing for vercel

--- api/index.py
+++ api/index.py
@@ -1,3 +1,11 @@
+import os
+import sys
+from pathlib import Path
+
+# Add project root to sys.path
+root = Path(__file__).resolve().parents[1]
+sys.path.append(str(root))
+
 from intelligence.main import app
 
 # This file is the entry point for Vercel Serverless Functions.
